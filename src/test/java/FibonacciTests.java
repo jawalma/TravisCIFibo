bf7287701e673f7f416700c7511c11f6817cded3
Jawad Al-Mamoon <jaalmamoon@bsu.edu>
2019-11-16 21:29:51 -0500

    Set up a test to fail

--- src/test/java/FibonacciTests.java
+++ src/test/java/FibonacciTests.java
@@ -5,10 +5,17 @@ import org.junit.Test;
 public class FibonacciTests {
 
     @Test
-    public void FibonacciTest(){
+    public void fibonacciTestpass(){
         Fibonacci fibo = new Fibonacci();
         int s = fibo.fib(5);
         Assert.assertEquals(s, 5);
+    }
+
+    @Test
+    public void fibonacciTestFail(){
+        Fibonacci fibo = new Fibonacci();
+        int s = fibo.fib(5);
+        Assert.assertEquals(s, 4);
     }
 
 }
