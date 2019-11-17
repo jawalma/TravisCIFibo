3754530d53700191de2d8583f90e4fb83dfc1d07
Jawad Al-Mamoon <jaalmamoon@bsu.edu>
2019-11-16 21:37:36 -0500

    Wat

--- src/test/java/FibonacciTests.java
+++ src/test/java/FibonacciTests.java
@@ -5,11 +5,15 @@ import org.junit.Test;
 public class FibonacciTests {
 
     @Test
-    public void fibonacciTestpass(){
+    public void fibonacciTestFail(){
         Fibonacci fibo = new Fibonacci();
         int s = fibo.fib(5);
         Assert.assertEquals(s, 4);
     }
 
+    @Test
+    public void failingatlife(){
+        Assert.assertTrue(false);
+    }
 
 }
